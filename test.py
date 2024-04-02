691f0d451a8e883f2c32892a17e85694cea22620
douzi-6 <75210407+douzi-6@users.noreply.github.com>
2024-04-02 10:05:25 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+# radius =float(input('请输入圆的半径 '))
+# per =2*3.14*radius
+# area =3.14*radius*radius
+# print('周长:%.2f'%per)
+# print('面积:%.2f'%area)
+
+usrname = input('请输入用户名：')
+psw = input('请输入口令：')
+usrname =='admin'and psw == '123'
+if usrname =='admin'and psw =='123':
+    print("验证成功")
+else:
+    print("验证失败")
